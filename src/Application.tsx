b58cb7102877c8842dde0af40c75160b68e15d54
konojack <konojack@gmail.com>
2022-09-29 19:48:00 +0200

    Refactor and adding types for ColorAdjustment

--- src/Application.tsx
+++ src/Application.tsx
@@ -2,26 +2,29 @@ import * as React from 'react';
 
 import { ColorSwatch } from './ColorSwatch';
 import { ColorInputs } from './ColorInputs';
-import { ColorSliders } from './ColorSliders';
+import { ColorAdjustment } from './ColorAdjustment';
+import { ColorSlider } from './ColorSlider';
 
 import { toRGB } from './utilities';
 import { reducer } from './reducer';
 import { ThemeContext } from './theme-context';
+import { RGBContext } from './context';
 
 const Application = () => {
   const { light: lightTheme, dark: darkTheme } = React.useContext(ThemeContext);
 
-  const [rgb, dispatch] = React.useReducer(reducer, {
-    red: 0,
-    green: 0,
-    blue: 0
-  });
+  const { red, green, blue } = React.useContext(RGBContext);
+  const rgbObject = {
+    red,
+    green,
+    blue
+  };
 
   return (
-    <main style={{ borderColor: toRGB(rgb), ...darkTheme }}>
+    <main style={{ borderColor: toRGB(rgbObject), ...darkTheme }}>
       <ColorSwatch />
-      <ColorInputs {...rgb} />
-      <ColorSliders />
+      <ColorInputs />
+      <ColorAdjustment Adjustment={ColorSlider} />
     </main>
   );
 };
